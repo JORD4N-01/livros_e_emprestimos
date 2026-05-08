bbceca9d38ddde3b78652cc5cbdf4bd26faa7878
Maria Clara Neves <nevesgomesclara6@gmail.com>
2026-05-08 13:42:56 -0400

    test: mockdatas para testar a API

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from routes.routes import register_routes
 def create_app():
     app = Flask(__name__)
 
-    # Registrar rotas (os devs vão implementar depois)
     register_routes(app)
 
     return app
